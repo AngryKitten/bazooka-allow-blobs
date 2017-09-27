f43edd96024d990e3ea2c2bfb7c1906bf63d86f7
M1038980 <m1038980@Phthisis.local>
2017-09-27 10:50:30 -0400

    Oh look here are some changes I got from master because why not?

---
+++ src/app/components/plots/generic/generic.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plot-generic',
+  templateUrl: './generic.component.html',
+  styleUrls: ['./generic.component.scss']
+})
+export class GenericComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
